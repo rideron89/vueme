da41d0e2e857976563606e68f03f0a58a6428bd9
Ron Rider <rideron89@gmail.com>
2017-05-26 15:06:08 -0400

    more fixes..hopefully

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './admin/dist'),
-    publicPath: './admin/dist/',
+    publicPath: '/admin/dist/',
     filename: 'build.js'
   },
   module: {
